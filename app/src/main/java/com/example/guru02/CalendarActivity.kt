18c601aa86bf42e6765b0dec627a66e47ab9312b
Moonsoyoon <msy1373@naver.com>
2023-01-17 17:21:57 +0900

    메모추가

--- app/src/main/java/com/example/guru02/CalendarActivity.kt
+++ app/src/main/java/com/example/guru02/CalendarActivity.kt
@@ -1,12 +1,15 @@
 package com.example.guru02
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
+import android.widget.EditText
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class CalendarActivity : AppCompatActivity() {
         var userID: String = "userID"
@@ -16,8 +19,10 @@ class CalendarActivity : AppCompatActivity() {
         lateinit var calendarTextView: TextView
         lateinit var title: TextView
         lateinit var plusBtn : Button
+        lateinit var editTextMemo : EditText
 
 
+        @SuppressLint("MissingInflatedId")
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_calendar)
@@ -27,6 +32,7 @@ class CalendarActivity : AppCompatActivity() {
             calendarTextView=findViewById(R.id.calendarTextView)
             title=findViewById(R.id.title)
             plusBtn = findViewById(R.id.plusBtn)
+            editTextMemo = findViewById(R.id.editTextMemo)
 
             title.text = "달력 일기장"
 
