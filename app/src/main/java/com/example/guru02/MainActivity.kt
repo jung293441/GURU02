200492a456d95cafc4110d47d4b272b14deec893
jeongyoon02 <kandch0514@naver.com>
2023-01-17 16:55:01 +0900

    Home 삭제, calendar 추가

--- app/src/main/java/com/example/guru02/MainActivity.kt
+++ app/src/main/java/com/example/guru02/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.guru02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, CalendarActivity::class.java)
+        var StartButton = findViewById<Button>(R.id.StartButton)
+
+        StartButton.setOnClickListener {
+            startActivity(intent)
+
+
+        }
     }
 }
