38c9441d838b36b6b6ccb9f982e6a1a4354e67fe
jeongyoon02 <kandch0514@naver.com>
2023-01-19 15:01:03 +0900

    메모 DB

--- app/src/main/java/com/example/guru02/CalendarActivity.kt
+++ app/src/main/java/com/example/guru02/CalendarActivity.kt
@@ -2,51 +2,76 @@ package com.example.guru02
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.Menu
 import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.EditText
 import android.widget.TextView
-import org.w3c.dom.Text
+import androidx.appcompat.app.AppCompatActivity
+
 
 class CalendarActivity : AppCompatActivity() {
-        lateinit var calendarView: CalendarView
-        lateinit var calendarTextView: TextView
-        lateinit var plusBtn : Button
-        lateinit var editTextMemo : EditText
+    lateinit var calendarView: CalendarView
+    lateinit var calendarTextView: TextView
+    lateinit var plusBtn: Button
+    lateinit var editTextMemo: EditText
+    lateinit var editMemoButtion: Button
 
 
-        @SuppressLint("MissingInflatedId")
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_calendar)
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
 
-            // UI값 생성
-            calendarView=findViewById(R.id.calendarView)
-            calendarTextView=findViewById(R.id.calendarTextView)
-            plusBtn = findViewById(R.id.plusBtn)
-            editTextMemo = findViewById(R.id.editTextMemo)
+        // UI값 생성
+        calendarView = findViewById(R.id.calendarView)
+        calendarTextView = findViewById(R.id.calendarTextView)
+        plusBtn = findViewById(R.id.plusBtn)
+        editTextMemo = findViewById(R.id.editTextMemo)
+        editMemoButtion = findViewById(R.id.editMemoButtion)
+
+        loadData()
 
 
-            calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-                calendarTextView.visibility = View.VISIBLE
-                plusBtn.visibility = View.VISIBLE
-                calendarTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
-                //checkDay(year, month, dayOfMonth, userID)
-            }
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            calendarTextView.visibility = View.VISIBLE
+            plusBtn.visibility = View.VISIBLE
+            calendarTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
+            //checkDay(year, month, dayOfMonth, userID)
+        }
 
-            val intent = Intent(this, ToDoList::class.java)
-            var plusBtn = findViewById<Button>(R.id.plusBtn)
+        val intent = Intent(this, ToDoList::class.java)
+        var plusBtn = findViewById<Button>(R.id.plusBtn)
 
-            plusBtn.setOnClickListener{
-                startActivity(intent)
-
-            }
+        plusBtn.setOnClickListener {
+            startActivity(intent)
 
         }
 
+        editMemoButtion.setOnClickListener{
+            saveData(editTextMemo.text.toString())
+            var intent = Intent(this, CalendarActivity::class.java)
+            intent.putExtra("memo", editTextMemo.text.toString())
+            startActivity(intent)
+        }
 
     }
+    private fun saveData(memo:String){
+        var pref = this.getPreferences(0)
+        var editor = pref.edit()
+
+        editor.putString("KEY_NAME", editTextMemo.text.toString()).apply()
+
+    }
+    private fun loadData(){
+        var pref = this.getPreferences(0)
+        var name = pref.getString("KEY_NAME", "")
+
+        editTextMemo.setText(name.toString())
+    }
+
+}
+
+
